c84de00572edc0eec1972b29f5caf79b6e5f511e
Alex <alex@Users-MacBook-Pro.local>
2019-08-05 11:50:23 +0300

    temp commit

--- src/js/Controllers/companies/companies.js
+++ src/js/Controllers/companies/companies.js
@@ -3,6 +3,7 @@
 // import Chart from 'chart.js';
 import CanvasJS from '../../Vendors/canvasjs.min';
 import Companies from '../../Models/CompaniesModel';
+import { handleResize } from '../../Helpers/helper';
 
 
 $( document ).ready( async function () {
@@ -27,10 +28,16 @@ $( document ).ready( async function () {
     });
 
 
-    (new CanvasJS.Chart("companiesChart", {
+    let width = chartContainer.innerWidth();
+    console.log(width);
+    let height = chartContainer.height();
+
+    let chart = new CanvasJS.Chart("companiesChart", {
+        width: 500,
+        height: height,
         theme: "light2", // "light1", "light2", "dark1", "dark2"
-        exportEnabled: true,
-        animationEnabled: true,
+        // exportEnabled: true,
+        // animationEnabled: true,
         data: [{
             click: function( chartItem ) {
                 console.log(chartItem);
@@ -45,12 +52,41 @@ $( document ).ready( async function () {
             toolTipContent: "<b>{label}</b>: {y}%",
             showInLegend: false,
             indexLabel: "{label} - {y}%",
-            indexLabelFontSize: 16,
+            indexLabelFontSize: 12,
+            indexLabelOrientation: 'horizontal',
+            indexLabelLineThickness: 1,
+            // indexLabelMaxWidth: 2,
             dataPoints: companies.chartPoints,
             explodeOnClick: false
         }]
-    })).render();
+    });
 
+    chart.render();
+
+    window.addEventListener("resize", handleResize);
+    function handleResize(  ) {
+        var w = window.innerWidth-2; // -2 accounts for the border
+        var h = window.innerHeight-2;
+        stage.canvas.width = w;
+        stage.canvas.height = h;
+        //
+        var ratio = 100/100; // 100 is the width and height of the circle content.
+        var windowRatio = w/h;
+        var scale = w/100;
+        if (windowRatio > ratio) {
+            scale = h/100;
+        }
+        // Scale up to fit width or height
+        c.scaleX= c.scaleY = scale;
+
+        // Center the shape
+        c.x = w / 2;
+        c.y = h / 2;
+
+        chart.render();
+    }
+
+    $(window).on('resize', handleResize);
 
     companiesContainer.on("click", ".list-group-item" ,function (e) {
         e.preventDefault();
